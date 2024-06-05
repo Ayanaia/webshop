1b6e3e67213399f83c4e534542d0e0e66b2a5ee2
JOUWGEBRUIKERSNAAM <ayah.kardesaltememi@student.arteveldehs.be>
2024-06-05 22:32:21 +0200

    render fix

--- app/app/src/services/api.tsx
+++ app/app/src/services/api.tsx
@@ -1,17 +1,16 @@
-import axios from 'axios';
+import axios from 'axios'
 
 const api = axios.create({
-    baseURL: 'http://localhost:3002/', 
-});
+  baseURL: 'http://localhost:3002/',
+})
 
 api.interceptors.request.use((config) => {
-    const token = localStorage.getItem('token');
-    if (token && !config.url?.includes('/login') && !config.url?.includes('/register')) {
-        config.headers.Authorization = `Bearer ${token}`;
-    }
-    console.log('Request Config:', config); // Log the request config to debug
-    return config;
-});
+  const token = localStorage.getItem('token')
 
-export default api;
+  config.headers.Authorization = `Bearer ${token}`
 
+  console.log('Request Config:', config) // Log the request config to debug
+  return config
+})
+
+export default api
